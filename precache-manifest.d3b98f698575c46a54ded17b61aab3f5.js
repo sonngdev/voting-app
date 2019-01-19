2d57794c94210bb97891c4a376767bc6aed76d14
nguyenthanhson <son.thanh.97@gmail.com>
2019-01-20 00:15:10 +0700

    Updates

---
+++ precache-manifest.d3b98f698575c46a54ded17b61aab3f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d43f34c0f8ca9c55a87",
+    "url": "/fcc-voting-app/static/js/runtime~main.8d43f34c.js"
+  },
+  {
+    "revision": "9477995d4299bb7e0889",
+    "url": "/fcc-voting-app/static/js/main.9477995d.chunk.js"
+  },
+  {
+    "revision": "88f5a14957300463ebb9",
+    "url": "/fcc-voting-app/static/js/1.88f5a149.chunk.js"
+  },
+  {
+    "revision": "9477995d4299bb7e0889",
+    "url": "/fcc-voting-app/static/css/main.fb20e327.chunk.css"
+  },
+  {
+    "revision": "88f5a14957300463ebb9",
+    "url": "/fcc-voting-app/static/css/1.55256f04.chunk.css"
+  },
+  {
+    "revision": "f18d2c66c3cceb861d119a6c3fda9d41",
+    "url": "/fcc-voting-app/index.html"
+  }
+];
