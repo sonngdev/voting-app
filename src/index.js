2fc45072756fc94795f6522fc4a0d585ef011dcf
nguyenthanhson <son.thanh.97@gmail.com>
2019-01-20 11:25:32 +0700

    Undo ignore index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 require('dotenv').config();
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter basename="/fcc-voting-app"><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
