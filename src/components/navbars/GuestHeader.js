90e525a32f3dc4ab9ac84ce3332d3b55ca3bc720
Nguyễn Thanh Sơn <thanhsonng.211@gmail.com>
2019-11-24 00:53:51 +0700

    Remove FCC in app name

--- src/components/navbars/GuestHeader.js
+++ src/components/navbars/GuestHeader.js
@@ -5,7 +5,7 @@ class GuestHeader extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-md navbar-light bg-light">
-        <Link to="/" className="navbar-brand">FCC Voting App</Link>
+        <Link to="/" className="navbar-brand">Voting App</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
